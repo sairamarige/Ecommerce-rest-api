0d316c67ea4ba9173b45841373bc222c20458c68
sairam <sairamarige2140@gmail.com>
2026-08-09 11:36:09 +0530

    Fix database URL casing for Ecommerce

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:1234@localhost/ecommerce"
+DATABASE_URL = "mysql+pymysql://root:1234@localhost/Ecommerce"
 
 engine = create_engine(DATABASE_URL)
 
